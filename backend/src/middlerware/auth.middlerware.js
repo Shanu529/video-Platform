22158def216cf0fbb2834cf9d0a1f488446c5c6e
Shanu Chhetri <shnau.chhetri529@gmail.com>
2025-10-21 10:59:53 +0530

    backend

--- backend/src/middlerware/auth.middlerware.js
+++ backend/src/middlerware/auth.middlerware.js
@@ -32,7 +32,8 @@ export const authUserMiddlerware = async (req, res, next) => {
 
 
 
-    const token = req.cookies.token;
+    const token =
+        req.cookies.token || req.headers.authorization?.split(" ")[1];
 
 
     if (!token) {
